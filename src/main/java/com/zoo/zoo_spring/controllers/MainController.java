f206a27be0947da850b77d16bf30e15c25e6c721
SidereaH <hutornoyaa@gmail.com>
2024-11-08 19:53:08 +0300

    the last changes on lesson

--- src/main/java/com/zoo/zoo_spring/controllers/MainController.java
+++ src/main/java/com/zoo/zoo_spring/controllers/MainController.java
@@ -4,15 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
 import com.zoo.zoo_spring.dal.DataAccessLayer;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:8080/")
 @RequestMapping("/unauthorized")
+@SessionAttributes("zoo")
 public class MainController {
     private final DataAccessLayer dataAccessLayer;
 
-    @Autowired
     public MainController(DataAccessLayer dataAccessLayer) {
         this.dataAccessLayer = dataAccessLayer;
     }
--- src/main/java/com/zoo/zoo_spring/dal/DataAccessLayer.java
+++ src/main/java/com/zoo/zoo_spring/dal/DataAccessLayer.java
@@ -1,21 +1,22 @@
 package com.zoo.zoo_spring.dal;
 
-import javax.crypto.NullCipher;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.hibernate.*;
 
-@Component
+import jakarta.persistence.EntityManager;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+
 public class DataAccessLayer {
 
     private final SessionFactory sessionFactory;
 
-    @Autowired
     public DataAccessLayer(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
 
     }
 
     Session session = null;
+
 }
